attester/attester-results-ui

commit f75576a1dac789f8c08734725906550953037048

Refactoring taskInfoModal as a function.

app/tasksTable/tasksTable.js
@@ -14,7 +14,8 @@
  */
 
 angular.module("attesterTasksTable", ["attesterTaskInfoModal", "attesterExecutionStates", "exportFile"]).directive("tasksTable", [
-        "$modal", "AttesterExecutionStates", "exportFile", function ($modal, executionStates, exportFile) {
+        "attesterTaskInfoModal", "AttesterExecutionStates", "exportFile",
+        function (attesterTaskInfoModal, executionStates, exportFile) {
 
             return {
                 restrict : "E",
@@ -163,16 +164,9 @@ angular.module("attesterTasksTable", ["attesterTaskInfoModal", "attesterExecutio
                     this.taskClick = function (task, browser) {
                         var browserTask = task.browsers[browser.name];
                         if (browserTask) {
-                            $modal.open({
-                                templateUrl : '/taskInfo/taskInfoModal.html',
-                                controllerAs : "ctrl",
-                                controller : "AttesterTaskInfoModalController",
-                                resolve : {
-                                    task : function () {
-                                        return browserTask;
-                                    }
-                                },
-                                size : "lg"
+                            attesterTaskInfoModal({
+                                task : browserTask,
+                                campaign : $scope.campaign
                             });
                         }
                     };

app/taskInfo/taskInfo.js
@@ -13,14 +13,15 @@
  * limitations under the License.
  */
 
-angular.module("attesterTaskInfo", ["attesterExecutionStates"]).directive("taskInfo", ["AttesterExecutionStates",
-        function (executionStates) {
+angular.module("attesterTaskInfo", ["attesterExecutionStates", "attesterTestsDetails"]).directive("taskInfo", [
+        "AttesterExecutionStates", function (executionStates) {
 
             return {
                 restrict : "E",
                 templateUrl : "/taskInfo/taskInfo.html",
                 scope : {
-                    task : "=task"
+                    task : "=",
+                    campaign : "="
                 },
                 controllerAs : "ctrl",
                 controller : ["$scope", function ($scope) {
@@ -48,8 +49,7 @@ angular.module("attesterTaskInfo", ["attesterExecutionStates"]).directive("taskI
                     this.getDuration = function (execution) {
                         var startedTime = execution.started.time;
                         var finished = execution.finished || {
-                            /* Using Math.floor to prevent angular 'Infinite $digest Loop' error */
-                            time : Math.max(startedTime, Math.floor(Date.now() / 1000) * 1000)
+                            time : $scope.campaign.lastUpdate
                         };
                         return finished.time - startedTime;
                     };

app/taskInfo/taskInfoModal.html
@@ -21,7 +21,7 @@
 	</h4>
 </div>
 <div class="modal-body">
-	<task-info task="ctrl.task"></task-info>
+	<task-info task="ctrl.task" campaign="ctrl.campaign"></task-info>
 </div>
 <div class="modal-footer">
 	<button type="button" class="btn btn-default" ng-click="$close()">Close</button>

app/taskInfo/taskInfoModal.js
@@ -13,7 +13,24 @@
  * limitations under the License.
  */
 
-angular.module("attesterTaskInfoModal", ["attesterTaskInfo"]).controller("AttesterTaskInfoModalController", ["task",
-        function (task) {
-            this.task = task;
+angular.module("attesterTaskInfoModal", ["attesterTaskInfo"]).factory("attesterTaskInfoModal", ["$modal",
+        function ($modal) {
+            var taskInfoModalController = ["config", function (config) {
+                        this.task = config.task;
+                        this.campaign = config.campaign;
+                    }];
+
+            return function (config) {
+                $modal.open({
+                    templateUrl : '/taskInfo/taskInfoModal.html',
+                    controllerAs : "ctrl",
+                    controller : taskInfoModalController,
+                    resolve : {
+                        config : function () {
+                            return config;
+                        }
+                    },
+                    size : "lg"
+                });
+            }
         }]);

commit ecff543e6873463fbf921223922cd17dfddbe3cc

Links to tests were no longer working.
This regression was introduced in f75576a1dac789f8c08734725906550953037048.

app/tasksTable/tasksTable.js
@@ -39,7 +39,7 @@ angular.module("attesterTasksTable", ["attesterTaskInfoModal", "attesterExecutio
                     browsersArray : "=",
                     browsersMap : "=",
                     tasksGroups : "=",
-                    testURL : "="
+                    testURL : "=testUrl"
                 },
                 controllerAs : "ctrl",
                 controller : ["$scope", function ($scope) {
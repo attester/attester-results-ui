ecff543e6873463fbf921223922cd17dfddbe3cc
ddivernois <david-emmanuel.divernois@amadeus.com>
2014-10-23 13:44:27 +0200

    Links to tests were no longer working.
    This regression was introduced in f75576a1dac789f8c08734725906550953037048.

--- app/tasksTable/tasksTable.js
+++ app/tasksTable/tasksTable.js
@@ -39,7 +39,7 @@ angular.module("attesterTasksTable", ["attesterTaskInfoModal", "attesterExecutio
                     browsersArray : "=",
                     browsersMap : "=",
                     tasksGroups : "=",
-                    testURL : "="
+                    testURL : "=testUrl"
                 },
                 controllerAs : "ctrl",
                 controller : ["$scope", function ($scope) {

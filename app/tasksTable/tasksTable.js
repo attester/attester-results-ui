attester/attester-results-ui

commit 23520fe294e2c70b696c2d4d624bacb365db4d0e

Displaying total durations

app/tasksTable/tasksTable.js
@@ -26,6 +26,30 @@ angular.module("attesterTasksTable", ["attesterTaskInfoModal", "attesterExecutio
         function (attesterTaskInfoModal, executionStates, exportFile, attesterCampaignsManager,
                 mergeCampaignsConfigService) {
 
+            var getExecutionDuration = function (execution) {
+                if (execution.started && execution.finished) {
+                    return execution.finished.time - execution.started.time;
+                }
+            };
+
+            var zeroForEachTaskState = function () {
+                return {
+                    error : 0,
+                    success : 0,
+                    waiting : 0,
+                    ignored : 0,
+                    total : 0
+                };
+            };
+
+            var appendForEachTaskState = function (total, partial) {
+                total.error += partial.error;
+                total.success += partial.success;
+                total.waiting += partial.waiting;
+                total.ignored += partial.ignored;
+                total.total += partial.total;
+            };
+
             var sameCampaignHeaders = function (array1, array2) {
                 var l = array1.length;
                 if (l != array2.length) {
@@ -69,6 +93,8 @@ angular.module("attesterTasksTable", ["attesterTaskInfoModal", "attesterExecutio
                         state.pageSize = 10;
                         state.currentPage = 1;
                         state.currentSortOrder = null;
+                        state.showRealDurations = false;
+                        state.showIdealDurations = false;
                         state.initialized = true;
                     }
                     this.tasksNumber = 0;
@@ -168,33 +194,48 @@ angular.module("attesterTasksTable", ["attesterTaskInfoModal", "attesterExecutio
                                 return isIncluded;
                             });
                         }
-                        var counts = this.counts = {};
-                        var totalCount = counts.total = {
-                            error : 0,
-                            success : 0,
-                            waiting : 0,
-                            ignored : 0,
-                            total : 0
-                        };
+                        // ideal = only take one execution per task into account
+                        // real = take all executions into account
+                        var idealCount = this.idealCount = {};
+                        var realCount = this.realCount = {};
+                        var idealDuration = this.idealDuration = {};
+                        var realDuration = this.realDuration = {};
+                        var totalIdealCount = idealCount.total = zeroForEachTaskState();
+                        var totalRealCount = realCount.total = zeroForEachTaskState();
+                        var totalIdealDuration = idealDuration.total = zeroForEachTaskState();
+                        var totalRealDuration = realDuration.total = zeroForEachTaskState();
+
                         state.visibleBrowsers.forEach(function (curBrowser) {
-                            var curBrowserCount = {
-                                error : 0,
-                                success : 0,
-                                waiting : 0,
-                                ignored : 0,
-                                total : 0
-                            };
+                            var curBrowserIdealCount = idealCount[curBrowser.browserKey] = zeroForEachTaskState();
+                            var curBrowserRealCount = realCount[curBrowser.browserKey] = zeroForEachTaskState();
+                            var curBrowserIdealDuration = idealDuration[curBrowser.browserKey] = zeroForEachTaskState();
+                            var curBrowserRealDuration = realDuration[curBrowser.browserKey] = zeroForEachTaskState();
                             res.forEach(function (taskGroup) {
-                                var curTaskState = executionStates.getTaskState(taskGroup.browsers[curBrowser.browserKey]);
-                                curBrowserCount[curTaskState]++;
-                                curBrowserCount.total++;
+                                var task = taskGroup.browsers[curBrowser.browserKey];
+                                var curTaskExecution = executionStates.getExecution(task);
+                                var curTaskState = executionStates.getExecutionState(curTaskExecution);
+                                curBrowserIdealCount[curTaskState]++;
+                                curBrowserIdealCount.total++;
+                                var curTaskDuration = getExecutionDuration(curTaskExecution);
+                                if (curTaskDuration > 0) {
+                                    curBrowserIdealDuration[curTaskState] += curTaskDuration;
+                                    curBrowserIdealDuration.total += curTaskDuration;
+                                }
+                                task.executions.forEach(function (execution) {
+                                    var executionState = executionStates.getExecutionState(execution);
+                                    curBrowserRealCount[executionState]++;
+                                    curBrowserRealCount.total++;
+                                    var duration = getExecutionDuration(execution);
+                                    if (duration > 0) {
+                                        curBrowserRealDuration[executionState] += duration;
+                                        curBrowserRealDuration.total += duration;
+                                    }
+                                });
                             });
-                            counts[curBrowser.browserKey] = curBrowserCount;
-                            totalCount.error += curBrowserCount.error;
-                            totalCount.success += curBrowserCount.success;
-                            totalCount.waiting += curBrowserCount.waiting;
-                            totalCount.ignored += curBrowserCount.ignored;
-                            totalCount.total += curBrowserCount.total;
+                            appendForEachTaskState(totalIdealCount, curBrowserIdealCount);
+                            appendForEachTaskState(totalRealCount, curBrowserRealCount);
+                            appendForEachTaskState(totalIdealDuration, curBrowserIdealDuration);
+                            appendForEachTaskState(totalRealDuration, curBrowserRealDuration);
                         });
                         this.tasksNumber = res.length;
                         var currentSortOrder = state.currentSortOrder;
@@ -290,6 +331,8 @@ angular.module("attesterTasksTable", ["attesterTaskInfoModal", "attesterExecutio
                         }
                     };
 
+                    this.getStateIcon = executionStates.getStateIcon;
+
                     this.getFilterClass = function (browser, filterState) {
                         var filterTestState = state.filterTestState;
                         var res = executionStates.getStateIcon(filterState);
@@ -319,12 +362,8 @@ angular.module("attesterTasksTable", ["attesterTaskInfoModal", "attesterExecutio
 
                     this.getDuration = function (task, browser) {
                         var execution = executionStates.getExecution(task.browsers[browser.browserKey]);
-                        if (execution && execution.started) {
-                            if (execution.finished) {
-                                return (execution.finished.time - execution.started.time);
-                            }
-                        }
-                        return -1;
+                        var duration = getExecutionDuration(execution);
+                        return duration >= 0 ? duration : -1;
                     };
                     this.sortOrders.push({
                         name : "Duration",

app/tasksTable/tasksTable.html
@@ -73,18 +73,18 @@
 			<th ng-repeat="browser in state.visibleBrowsers" class="browser-header">{{browser.name}}</th>
 		</tr>
 		<tr>
-			<th><uib-progress max="1000" animate="false" ng-if="ctrl.counts.total.total != 0">
-						<uib-bar type="success" ng-if="ctrl.counts.total.success != 0" value="1000 * ctrl.counts.total.success / ctrl.counts.total.total" title="{{ctrl.counts.total.success}} successful tasks"></uib-bar>
-						<uib-bar type="danger" ng-if="ctrl.counts.total.error != 0" value="1000 * ctrl.counts.total.error / ctrl.counts.total.total" title="{{ctrl.counts.total.error}} failed tasks"></uib-bar>
-						<uib-bar type="warning" ng-if="ctrl.counts.total.ignored != 0" value="1000 * ctrl.counts.total.ignored / ctrl.counts.total.total" title="{{ctrl.counts.total.ignored}} ignored tasks"></uib-bar>
+			<th><uib-progress max="1000" animate="false" ng-if="ctrl.idealCount.total.total != 0">
+						<uib-bar type="success" ng-if="ctrl.idealCount.total.success != 0" value="1000 * ctrl.idealCount.total.success / ctrl.idealCount.total.total" title="{{ctrl.idealCount.total.success}} successful tasks"></uib-bar>
+						<uib-bar type="danger" ng-if="ctrl.idealCount.total.error != 0" value="1000 * ctrl.idealCount.total.error / ctrl.idealCount.total.total" title="{{ctrl.idealCount.total.error}} failed tasks"></uib-bar>
+						<uib-bar type="warning" ng-if="ctrl.idealCount.total.ignored != 0" value="1000 * ctrl.idealCount.total.ignored / ctrl.idealCount.total.total" title="{{ctrl.idealCount.total.ignored}} ignored tasks"></uib-bar>
 				</uib-progress>
-				<uib-progress max="1000" animate="false" ng-if="ctrl.counts.total.total == 0"></uib-progress>
+				<uib-progress max="1000" animate="false" ng-if="ctrl.idealCount.total.total == 0"></uib-progress>
 			</th>
 			<th ng-repeat="browser in state.visibleBrowsers" class="browser-header">
-				<uib-progress max="1000" animate="false" ng-if="ctrl.counts[browser.browserKey].total != 0">
-						<uib-bar type="success" ng-if="ctrl.counts[browser.browserKey].success != 0" value="1000 * ctrl.counts[browser.browserKey].success / ctrl.counts[browser.browserKey].total" title="{{ctrl.counts[browser.browserKey].success}} successful tasks"></uib-bar>
-						<uib-bar type="danger" ng-if="ctrl.counts[browser.browserKey].error != 0" value="1000 * ctrl.counts[browser.browserKey].error / ctrl.counts[browser.browserKey].total" title="{{ctrl.counts[browser.browserKey].error}} failed tasks"></uib-bar>
-						<uib-bar type="warning" ng-if="ctrl.counts[browser.browserKey].ignored != 0" value="1000 * ctrl.counts[browser.browserKey].ignored / ctrl.counts[browser.browserKey].total" title="{{ctrl.counts[browser.browserKey].ignored}} ignored tasks"></uib-bar>
+				<uib-progress max="1000" animate="false" ng-if="ctrl.idealCount[browser.browserKey].total != 0">
+						<uib-bar type="success" ng-if="ctrl.idealCount[browser.browserKey].success != 0" value="1000 * ctrl.idealCount[browser.browserKey].success / ctrl.idealCount[browser.browserKey].total" title="{{ctrl.idealCount[browser.browserKey].success}} successful tasks"></uib-bar>
+						<uib-bar type="danger" ng-if="ctrl.idealCount[browser.browserKey].error != 0" value="1000 * ctrl.idealCount[browser.browserKey].error / ctrl.idealCount[browser.browserKey].total" title="{{ctrl.idealCount[browser.browserKey].error}} failed tasks"></uib-bar>
+						<uib-bar type="warning" ng-if="ctrl.idealCount[browser.browserKey].ignored != 0" value="1000 * ctrl.idealCount[browser.browserKey].ignored / ctrl.idealCount[browser.browserKey].total" title="{{ctrl.idealCount[browser.browserKey].ignored}} ignored tasks"></uib-bar>
 				</uib-progress></th>
 		</tr>
 		<tr>
@@ -144,4 +144,64 @@
 				ng-switch-when="errorMsgs">{{ ctrl.getErrorMsgs(task, browser) }}</span></td>
 		</tr>
 	</tbody>
+	<tfoot>
+		<tr ng-if="!state.showIdealDurations">
+			<th><a href="#" ng-click="state.showIdealDurations = true; $event.preventDefault()">Show durations (1 execution/task)</a></th>
+		</tr>
+		<tr ng-if="state.showIdealDurations && (ctrl.idealDuration.total.success != 0 || ctrl.idealDuration.total.error != 0)">
+			<th>
+				<a href="#" ng-click="state.showIdealDurations = false; $event.preventDefault()">Hide</a><br>
+				Duration (1 execution / task): {{ ctrl.idealDuration.total.total | duration }}
+			</th>
+			<th ng-repeat="browser in state.visibleBrowsers">
+				<br>
+				{{ ctrl.idealDuration[browser.browserKey].total | duration }}
+			</th>
+		</tr>
+		<tr ng-if="state.showIdealDurations && ctrl.idealDuration.total.success != 0" class="success">
+			<td>
+				<span ng-class="ctrl.getStateIcon('success')"></span> {{ ctrl.idealDuration.total.success | duration }}
+			</td>
+			<td ng-repeat="browser in state.visibleBrowsers">
+				{{ ctrl.idealDuration[browser.browserKey].success | duration }}
+			</td>
+		</tr>
+		<tr ng-if="state.showIdealDurations && ctrl.idealDuration.total.error != 0" class="danger">
+			<td>
+				<span ng-class="ctrl.getStateIcon('error')"></span> {{ ctrl.idealDuration.total.error | duration }}
+			</td>
+			<td ng-repeat="browser in state.visibleBrowsers">
+				{{ ctrl.idealDuration[browser.browserKey].error | duration }}
+			</td>
+		</tr>
+		<tr ng-if="!state.showRealDurations">
+			<th><a href="#" ng-click="state.showRealDurations = true; $event.preventDefault()">Show durations (all executions)</a></th>
+		</tr>
+		<tr ng-if="state.showRealDurations && (ctrl.realDuration.total.success != 0 || ctrl.realDuration.total.error != 0)">
+			<th>
+				<a href="#" ng-click="state.showRealDurations = false; $event.preventDefault()">Hide</a><br>
+				Total duration (all executions): {{ ctrl.realDuration.total.total | duration }}
+			</th>
+			<th ng-repeat="browser in state.visibleBrowsers">
+				<br>
+				{{ ctrl.realDuration[browser.browserKey].total | duration }}
+			</th>
+		</tr>
+		<tr ng-if="state.showRealDurations && ctrl.realDuration.total.success != 0" class="success">
+			<td>
+				<span ng-class="ctrl.getStateIcon('success')"></span> {{ ctrl.realDuration.total.success | duration }}
+			</td>
+			<td ng-repeat="browser in state.visibleBrowsers">
+				{{ ctrl.realDuration[browser.browserKey].success | duration }}
+			</td>
+		</tr>
+		<tr ng-if="state.showRealDurations && ctrl.realDuration.total.error != 0" class="danger">
+			<td>
+				<span ng-class="ctrl.getStateIcon('error')"></span> {{ ctrl.realDuration.total.error | duration }}
+			</td>
+			<td ng-repeat="browser in state.visibleBrowsers">
+				{{ ctrl.realDuration[browser.browserKey].error | duration }}
+			</td>
+		</tr>
+	</tfoot>
 </table>

commit 5ab42e637c23068795f4994969262a9aee10b672

Fixing error when comparing results with some tasks not present in all results
This issue was probably a regression introduced in 23520fe294e2c70b696c2d4d624bacb365db4d0e

app/tasksTable/tasksTable.js
@@ -212,6 +212,9 @@ angular.module("attesterTasksTable", ["attesterTaskInfoModal", "attesterExecutio
                             var curBrowserRealDuration = realDuration[curBrowser.browserKey] = zeroForEachTaskState();
                             res.forEach(function (taskGroup) {
                                 var task = taskGroup.browsers[curBrowser.browserKey];
+                                if (!task) {
+                                    return;
+                                }
                                 var curTaskExecution = executionStates.getExecution(task);
                                 var curTaskState = executionStates.getExecutionState(curTaskExecution);
                                 curBrowserIdealCount[curTaskState]++;